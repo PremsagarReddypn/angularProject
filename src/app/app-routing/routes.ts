213e491c84d2db9b92a4aea87e93487ed7d45ef8
palamapalli premsagar reddy <premsagarreddypn@gmail.com>
2020-05-23 12:36:19 +0530

    single page application part-2 and assessment-2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,6 +10,8 @@ import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 export const routes: Routes = [
     {path : 'home', component: HomeComponent},
     {path : 'menu', component: MenuComponent},
+    {path : 'dishdetail/:id', component: DishdetailComponent},
+    {path : 'aboutus', component: AboutComponent},
     {path : 'contactus', component: ContactComponent},
     {path : '', redirectTo:'/home',pathMatch:'full'}
 ];
